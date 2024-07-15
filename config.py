fb6f3bd5dec7b7ef487540b82f811e08e1f7e509
Waste1and <110408954+Waste1and@users.noreply.github.com>
2024-07-15 02:29:58 -0400

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ import os
 # Load environment variables from .env file
 load_dotenv()
 
-DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")  # Discord bot token
-PRIVATE_KEY = os.getenv("PRIVATE_KEY")  # Solana private key
-API_URL = os.getenv("API_URL")  # Solana API URL
+PRIVATE_KEY = os.getenv("PRIVATE_KEY")
+API_URL = os.getenv("API_URL")
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
