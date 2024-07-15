08ef32ed6e23443edf5fcc76d9a55388b3c204ee
Waste1and <110408954+Waste1and@users.noreply.github.com>
2024-07-15 02:27:38 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,11 @@
 # config.py
-PRIVATE_KEY = "your-private-key"
-API_URL = "https://api.mainnet-beta.solana.com"  # Solana API URL
+
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
+
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")  # Discord bot token
+PRIVATE_KEY = os.getenv("PRIVATE_KEY")  # Solana private key
+API_URL = os.getenv("API_URL")  # Solana API URL
